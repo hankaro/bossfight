74f0bf3a9e76ae5c3c1c3d079473a4666e04f0de
Hanna Korhonen <hanna.rk.korhonen@gmail.com>
2024-05-03 14:17:00 +0300

    Dice and Spellcasting functions, visual improvements to the page

--- script/boss.js
+++ script/boss.js
@@ -1,12 +1,42 @@
-var SpellSlots = 2; // Siirrä alustus silmukan ulkopuolelle
-var bossHP = 50
-var heroHP = 50
+var bossSpellSlots = 2;
+var heroSpellSlots = 2;
+var bossHP = 50;
+var heroHP = 50;
+
+function hitDice() {
+    var roll = Math.ceil(Math.random() * 20)
+    if (roll >= 10) {
+        return true
+    }
+    else {
+        return false
+    }
+    
+}
+
+function dmgDiceMelee() {
+    return Math.ceil(Math.random() * 8)
+}
+
+function dmgDiceSpell() {
+    return Math.ceil(Math.random() * 16)
+}
 
 
 function CastSpell() {
-    if (SpellSlots > 0) {
+    if (heroSpellSlots > 0) {
         console.log("Casting spell")
-        SpellSlots -= 1
+        var hits = hitDice()
+        if (hits) {
+            var dmg = dmgDiceSpell()
+            console.log("Spell hit for " + dmg + " damage")
+            bossHP -= dmg
+            console.log("Boss HP: " + bossHP)
+        }
+        else {
+            console.log("Spell missed")
+        }
+        heroSpellSlots -= 1
     }
     else {
         console.log("Not enough spell slots to cast spell")
@@ -19,3 +49,8 @@ function mainFight() {
     }
 }
 
+while (heroSpellSlots > 0) {
+    CastSpell ()
+    console.log("Boss HP: " + bossHP)
+    console.log("Spellslots: " + heroSpellSlots)
+}
